0834007ab3675a64c5667077807a6b0caa9b61a2
Arya Bhat <arubhat1528@gmail.com>
2026-07-06 23:56:46 +0530

    Production database config

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-    MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
-    MYSQL_USER = os.environ.get("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "Aryabhat@10")
-    MYSQL_DB = os.environ.get("MYSQL_DB", "internship_ai")
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
